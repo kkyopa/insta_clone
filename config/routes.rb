7739c9ebebedc0ee80e648bfe1fce44765690ee9
川田　京助 <kkyopa@gmail.com>
2018-09-17 12:00:06 +0000

    destroyのパスを変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
     resources :contacts
     resources :tuita do
-    collection do
-    post :confirm
-    end
+      collection do
+        post :confirm
+      end
     end
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
